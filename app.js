1887884aab587cb35ef59558325979182fe5d7d2
Thomas Trebat <tjtrebat@gmail.com>
2018-07-30 23:18:21 -0400

    Added login.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +17,11 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'shhhh, very secret'
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,80 @@
 var express = require('express');
+var hash = require('pbkdf2-password')();
 var router = express.Router();
 
+var users = {
+  tj: { name: 'tj' }
+};
+
+hash({ password: 'foobar' }, function (err, pass, salt, hash) {
+  if (err) throw err;
+  users.tj.salt = salt;
+  users.tj.hash = hash;
+});
+
+function authenticate(name, pass, fn) {
+  if (!module.parent) console.log('authenticating %s:%s', name, pass);
+  var user = users[name];
+  if (!user) return fn(new Error('cannot find user'));
+  hash({ password: pass, salt: user.salt }, function (err, pass, salt, hash) {
+    if (err) return fn(err);
+    if (hash === user.hash) return fn(null, user);
+    fn(new Error('invalid password'));
+  });
+}
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Project 1' });
+});
+
+router.get('/restricted', restrict, function (req, res) {
+  res.send('Wahoo! restricted area, click to <a href="/logout">logout</a>');
+});
+
+router.get('/logout', function (req, res) {
+  req.session.destroy(function () {
+    res.redirect('/');
+  });
+});
+
+router.get('/login', function (req, res) {
+  var err = req.session.error;
+  var msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = '';
+  if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+  if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+  res.render('login');
+});
+
+router.post('/login', function (req, res) {
+  authenticate(req.body.username, req.body.password, function (err, user) {
+    if (user) {
+      req.session.regenerate(function () {
+        req.session.user = user;
+        req.session.success = 'Authenticated as ' + user.name
+          + ' click to <a href="/logout">logout</a>. '
+          + ' You may now access <a href="/restricted">/restricted</a>.';
+        res.redirect('back');
+      });
+    } else {
+      req.session.error = 'Authentication failed, please check your '
+        + ' username and password.'
+        + ' (use "tj" and "foobar")';
+      res.redirect('/login');
+    }
+  });
 });
 
 module.exports = router;
